63975c122835b42e999c9fffe03d217548323763
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-16 19:38:06 +0200

    старт

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import UserList from "./components/UserList";
-import users from './user.json';
-
-export default function App() {
-    return (<div>
-   <UserList items={users}/>
-    </div>);
-}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
 
-import App from './App';
-
-// 👇️ IMPORTANT: use correct ID of your root element
-// this is the ID of the div in your index.html file
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-// 👇️ if you use TypeScript, add non-null (!) assertion operator
-// const root = createRoot(rootElement!);
-
-root.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 );
