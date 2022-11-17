776b8a4f8167a118cf39e6e215f163719beebf3c
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-17 20:34:19 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,19 @@
 import Profile from './profile/Profile';
 import user from '../data/user.json';
+import Statistics from './statistics/Statistics';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
-    <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
   );
 };
