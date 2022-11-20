e071ecabb805c5aa547f82d7727158a98493f9f6
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-20 20:58:16 +0200

    add

---
+++ src/components/friend/FriendList.js
@@ -0,0 +1,16 @@
+import style from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+const FriendList = ({}) => {
+  return (
+    <ul className="friend-list">
+      <li className="item">
+        <span className="status"></span>
+        <img className="avatar" src="" alt="User avatar" width="48" />
+        <p className="name"></p>
+      </li>
+    </ul>
+  );
+};
+
+export default FriendList;
