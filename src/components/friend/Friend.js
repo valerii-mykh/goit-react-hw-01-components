c62ad47f2b460cdc4abca355efd56d70dacfea81
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-20 22:19:19 +0200

    add

---
+++ src/components/friend/Friend.js
@@ -0,0 +1,27 @@
+// import style from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+function Friend({ stats }) {
+  return (
+    <ul className="friend_list">
+      {stats.map(stat => (
+        <li className="item" key={stat.id}>
+          <span className="status">{stat.isOnline}</span>
+          <img className="avatar" src={stat.avatar} alt="User avatar" width="48" />
+          <p className="name">{stat.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+Friend.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string,
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string,
+      //   isOnline: PropTypes.bool,
+    })
+  ),
+};
+export default Friend;
