00af93a64d1cefb60221510efe87759157df4dcf
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-21 23:30:16 +0200

    add

--- src/components/friend/Friend.js
+++ src/components/friend/Friend.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export const Friend = ({ avatar, name, isOnline }) => {
   return (
     <li className="item">
-      <span className="status"></span>
+      <span className="status">{isOnline}</span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
     </li>
