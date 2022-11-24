90da456bd3ad022c9ed97f7c3e1bbf88915b18c3
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-24 13:30:57 +0200

    add

--- src/components/transactions/Transactions.js
+++ src/components/transactions/Transactions.js
@@ -1,6 +1,39 @@
-import style from './Transactions.module.css';
+import style from 'components/TransactionHistory/TransactionHistory.module.css';
 import PropTypes from 'prop-types';
 
-function Transitions({ items }) {}
-Transitions.propTypes = {};
-export default Transitions;
+function TransactionHistory({ items }) {
+  return (
+    <table className={style.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string,
+      amount: PropTypes.string,
+      currency: PropTypes.string,
+    })
+  ),
+};
+
+export default TransactionHistory;
